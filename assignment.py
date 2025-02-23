8968eefd57a2c1264d9f7cd7f20d94b459c6c57e
Nsohal0614 <nidhisohal@hotmail.com>
2025-02-23 12:24:08 -0500

    assignment.py

--- assignment.py
+++ assignment.py
@@ -13,3 +13,17 @@
 #The month is February and the current season is Winter.
 #Remember to also complete the flowchart. It is strongly advised that you do the flowchart first,
 #as this will help you write the code.
+
+
+month= int(input("What month is it? (1-12):"))
+
+if month in [3, 4, 5]:
+    season = "Spring"  
+elif month in [6, 7, 8]:
+    season = "Summer"
+elif month in [9,10,11]:
+    season = "Fall"
+else:
+    season = "Winter"
+    
+print(f"The current season is {season}.")
